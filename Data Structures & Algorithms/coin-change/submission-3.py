d8994ac463661ae9d45a4dda541bed2466dee7a0
Jordan J <jordan879upbeat@gmail.com>
2026-06-06 14:30:30 -0400

    Add: coin-change - submission-3

---
+++ Data Structures & Algorithms/coin-change/submission-3.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        q = deque()
+        q.append((amount, 0))
+
+        seen = set()
+        seen.add(amount)
+
+        while q:
+            amountRemaining, numCoins = q.popleft()
+
+            if amountRemaining == 0:
+                return numCoins
+
+            for coin in coins:
+                newAmount = amountRemaining - coin
+
+                if newAmount >= 0 and newAmount not in seen:
+                    seen.add(newAmount)
+                    q.append((newAmount, numCoins + 1))
+
+        return -1
